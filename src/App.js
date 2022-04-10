be4c40e3b06c86b472b48d425f1ce0a5d38d9c3a
AndreMP97 <andre.m.pacheco.97@gmail.com>
2022-04-10 02:28:04 +0100

    Scroll Fix

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import Result from './components/result';
 import Disclaimer from './components/disclaimer';
 import Nav from './components/nav';
 import Footer from './components/footer';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <BrowserRouter>
-      <Nav />
-      <Routes>
-        <Route path="weather-app/" element={<Home />} />
-        <Route path="weather-app/result" element={<Result />}/>
-        <Route path="weather-app/disclaimer" element={<Disclaimer />}/>
-      </Routes>
-      <Footer />
+      <ScrollToTop>
+        <Nav />
+        <Routes>
+          <Route path="weather-app/" element={<Home />} />
+          <Route path="weather-app/result" element={<Result />}/>
+          <Route path="weather-app/disclaimer" element={<Disclaimer />}/>
+        </Routes>
+        <Footer />
+      </ScrollToTop>
     </BrowserRouter>
   );
 }
